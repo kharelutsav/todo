dca9a5880ae3d7d12c0ff3cc758368ccbd724792
kharelutsav <kharelutsav4@gmail.com>
2021-12-30 18:45:24 +0545

    Add with severity and exiry. Work on update started.

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,14 +1,14 @@
 import '../css/add.css'
 import { useState } from 'react'
 import { FaPlus, FaExclamation } from 'react-icons/fa'
-// import { MdOutlineDownloadDone } from 'react-icons/md'
+import { FiPlus, FiMinus } from 'react-icons/fi'
 import { ImCross } from 'react-icons/im'
 
 function Add(props) {
     const [input, setInput] = useState('')
-    const [option, setOption] = useState('Low')
-    const [title, setTitle] = useState('')
     const [desc, setDesc] = useState('')
+    const [severity, setSeverity] = useState(1)
+    const [expiry, setExpiry] = useState(5)
     const [setData, setPortal] = props.state
 
     const time = new Date().toLocaleTimeString()
@@ -18,12 +18,11 @@ function Add(props) {
         if (input === "") {
             return alert('Input can not be empty.')
         }
-        const records = [input, date, time, option]
+        const records = [input, date, time, severity, expiry, desc]
         setData( rec => [...rec, records])
         setPortal('Display')
         // setInput('')
     }
-
     return (
         <div className='disp-container'>
             <div className='add-icons'>
@@ -37,10 +36,43 @@ function Add(props) {
                     <ImCross className='fa-plus icons' onClick={()=>setPortal('Display')}/>
                 </span>
             </div>
+
             <div className= 'disp-item'>
-                <input placeholder='Enter a new todo.' value={input} onInput={e => {setInput(e.target.value); setTitle(e.target.value);}} className='inp-todo'/>
-                <button className='button'>Add</button>
+                <input placeholder='Enter a new todo.' value={input} onInput={e => setInput(e.target.value)} className='inp-todo'/>
+                <span className='severity'>
+                    <p>Severity</p>
+                    <FiMinus className='fi-ic' onClick={()=>{
+                        if (severity > 1){
+                            return setSeverity(severity-1)
+                        }
+                        return
+                        }}/>
+                    <button id='severity' disabled>{severity}</button>
+                    <FiPlus className='fi-ic' onClick={()=>{
+                        if (severity < 5){
+                            return setSeverity(severity+1)
+                        }
+                        return
+                        }}/>
+                </span>
+                <span className='expire-on'>
+                    <p>Expire-on</p>
+                    <FiMinus className='fi-ic' onClick={()=>{
+                        if (expiry > 1){
+                            return setExpiry(expiry-1)
+                        }
+                        return
+                        }}/>
+                    <button id='expiry' disabled>{expiry} {expiry === 1 ? 'Day': 'Days'}</button>
+                    <FiPlus className='fi-ic' onClick={()=>{
+                        if (expiry < 14){
+                            return setExpiry(expiry+1)
+                        }
+                        return
+                        }}/>
+                </span>
             </div>
+
             <div className='disp-item'>
                 <textarea className='description-area' value={desc} onChange={e => setDesc(e.target.value)} placeholder='Descriptions of your task....'></textarea>
             </div>
--- src/components/Update.js
+++ src/components/Update.js
@@ -1,31 +1,86 @@
-import React from 'react'
-import { FcNext, FcPrevious } from 'react-icons/fc'
-import '../css/update.css'
+import '../css/add.css'
 import { useState } from 'react'
+import { FaPlus, FaExclamation } from 'react-icons/fa'
+import { FiPlus, FiMinus } from 'react-icons/fi'
+import { ImCross } from 'react-icons/im'
 
-function Update() {
-    const [title, setTitle] = useState('')
+function Add(props) {
+    const [input, setInput] = useState('')
     const [desc, setDesc] = useState('')
-    return (
-        <div className='Update'>
-            <header className='update-header'>
-                <FcPrevious className='prev-icon'/>
-                {title}
-                <FcNext className='next-icon'/>
-            </header>
+    const [severity, setSeverity] = useState(1)
+    const [expiry, setExpiry] = useState(5)
+    const [setData, setPortal] = props.state
 
-            <div>
-                <div className='update-description-info'>
-                    <input id='name' placeholder='Name of....' value={title} onChange={e => setTitle(e.target.value)}/>
-                    <textarea className='description-area' value={desc} onChange={e => setDesc(e.target.value)} placeholder='Descriptions of your task....'></textarea>
-                </div>
+    const time = new Date().toLocaleTimeString()
+    const date = new Date().toDateString()
+
+    const addData = () => {
+        if (input === "") {
+            return alert('Input can not be empty.')
+        }
+        const records = [input, date, time, severity, expiry, desc]
+        setData( rec => [...rec, records])
+        setPortal('Display')
+        // setInput('')
+    }
+    return (
+        <div className='disp-container'>
+            <div className='add-icons'>
+                <span>
+                    <FaPlus className='fa-plus icons' onClick={addData}/>
+                </span>
+                <span>
+                    <FaExclamation className='fa-plus icons'/>
+                </span>
+                <span>
+                    <ImCross className='fa-plus icons' onClick={()=>setPortal('Display')}/>
+                </span>
             </div>
 
-            <footer className='update-footer'>
-                Dedication. My foot.
-            </footer>
-        </div>
+            <div className= 'disp-item'>
+                <input placeholder='Enter a new todo.' value={input} onInput={e => setInput(e.target.value)} className='inp-todo'/>
+                <span className='severity'>
+                    <p>Severity</p>
+                    <FiMinus className='fi-ic' onClick={()=>{
+                        if (severity > 1){
+                            return setSeverity(severity-1)
+                        }
+                        return
+                        }}/>
+                    <button id='severity' disabled>{severity}</button>
+                    <FiPlus className='fi-ic' onClick={()=>{
+                        if (severity < 5){
+                            return setSeverity(severity+1)
+                        }
+                        return
+                        }}/>
+                </span>
+                <span className='expire-on'>
+                    <p>Expire-on</p>
+                    <FiMinus className='fi-ic' onClick={()=>{
+                        if (expiry > 1){
+                            return setExpiry(expiry-1)
+                        }
+                        return
+                        }}/>
+                    <button id='expiry' disabled>{expiry} {expiry === 1 ? 'Day': 'Days'}</button>
+                    <FiPlus className='fi-ic' onClick={()=>{
+                        if (expiry < 14){
+                            return setExpiry(expiry+1)
+                        }
+                        return
+                        }}/>
+                </span>
+            </div>
+
+            <div className='disp-item'>
+                <textarea className='description-area' value={desc} onChange={e => setDesc(e.target.value)} placeholder='Descriptions of your task....'></textarea>
+            </div>
+            <div className='disp-item'>
+                <p className='footer'>Dedication. My foot.</p>
+            </div>
+        </div>    
     )
 }
 
-export default Update
+export default Add
