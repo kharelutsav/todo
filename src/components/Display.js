8448ccb1bf2f309244b24f759a43354b1ec420b7
kharelutsav <kharelutsav4@gmail.com>
2021-12-31 12:59:49 +0545

    Mark as complete implemented

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/display.css'
 import { FaMinus, FaEdit, FaPlus } from 'react-icons/fa'
 import { MdOutlineDownloadDone } from 'react-icons/md'
+
+const Used = React.memo((props)=>{
+    const [rec, idx, data, setData, setIndex, setPortal] = props.props
+    const [marked, setMarked] = useState(rec[6]);
+    return (
+        <div className='disp-item'>
+            <div className='all-icons'>
+                <span className='span-ic'>
+                    {marked && <FaMinus className='fa-minus icons ic' onClick={()=>{setData(Data => {
+                                                                            let data = [...Data];
+                                                                            data.splice(idx, 1);
+                                                                            return [...data];
+                                                                        }); setIndex(idx);}}/>}
+                    {!marked && <FaMinus className='fa-minus ic' style={{color: "rgb(52, 156, 247)", backgroundColor: "lightgrey"}}/>}
+                </span>
+                <span className='span-ic'>
+                    {!marked && <FaEdit className='fa-edit icons ic' onClick={()=>{setPortal("Update"); setIndex(idx);}}/>}
+                    {marked && <FaEdit className='fa-edit icons ic' style={{backgroundColor: "lightgray"}}/>}
+                </span>
+                <span className='span-ic'>
+                    <MdOutlineDownloadDone className='md-done icons ic' onClick={()=>{
+                        setMarked(!marked);
+                        rec.splice(6,1,!marked)
+                        data.splice(idx, 1, rec);
+                        localStorage.setItem('add', JSON.stringify(data))
+                        }} style={{backgroundColor: marked ? "green" : ""}}/>
+                </span>
+            </div>
+            <span className="span-desc" title={rec[5]}>
+                <p className='todo-name'>{rec[0]}</p>
+                <p className='other-text'>{rec[1]} {rec[2]}</p>
+            </span>
+        </div>                      
+    )
+})
 
 function Display(props) {
     const [data, setData, setPortal] = props.state;
@@ -14,25 +49,8 @@ function Display(props) {
             </nav>
             {data.map((rec,idx) => {
                 return (
-                    <div className='disp-item' key={idx}>
-                        <div className='all-icons'>
-                            <span className='span-ic'>
-                                <FaMinus className='fa-minus icons ic' onClick={()=>{setData(Data => {
-                                                                                        let data = [...Data];
-                                                                                        data.splice(idx, 1);
-                                                                                        return [...data];
-                                                                                    }); setIndex(idx);}}/>
-                            </span>
-                            <span className='span-ic'>
-                                <FaEdit className='fa-edit icons ic' onClick={()=>{setPortal("Update"); setIndex(idx);}}/>
-                            </span>
-                            <span className='span-ic'>
-                                <MdOutlineDownloadDone className='md-done icons ic'/>
-                            </span>
-                        </div>                      
-                        <p className='todo-name'>{rec[0]}</p>
-                        <p className='other-text'>{rec[1]} {rec[2]}</p>
-                    </div>)
+                    <Used props={[rec, idx, data, setData, setIndex, setPortal]} key={idx}/>
+                )
             })}
         </div>
     )
